e28e5c1787d384c9076e9c5983a8e18cf4b64bda
e28121996 <dckelm@proton.me>
2024-11-02 03:05:37 +0800

    replit.nix update

--- replit.nix
+++ replit.nix
@@ -2,5 +2,8 @@
   deps = [
     pkgs.python311
     pkgs.python311Packages.pip
+    pkgs.python311Packages.flask
+    pkgs.python311Packages.telethon
+    pkgs.python311Packages.python-dotenv
   ];
 } 
